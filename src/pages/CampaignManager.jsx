1231a63601819524d351ec9f2a78547ded5c6104
shine <Gofunddonationplatformhelpneed@gmail.com>
2026-07-17 00:38:57 +0100

    Restore working CampaignManager skeleton

---
+++ src/pages/CampaignManager.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function CampaignManager() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "#fff",
+        padding: "30px",
+      }}
+    >
+      <h1 style={{ color: "#22d3ee" }}>Campaign Manager</h1>
+
+      <p>
+        Campaign Manager is being restored.
+      </p>
+    </div>
+  );
+}
